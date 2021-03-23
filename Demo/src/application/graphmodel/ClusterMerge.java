dd562a70a889715b2054e3b25211ad741a24b011
Riefer-Ba <80839493+Riefer-Ba@users.noreply.github.com>
2021-03-24 00:17:53 +0100

    Update ClusterMerge.java
    updated ,bis auf bug im cluster indexing funktioniert es!

--- Demo/src/application/graphmodel/ClusterMerge.java
+++ Demo/src/application/graphmodel/ClusterMerge.java
@@ -1,14 +1,23 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 public class ClusterMerge {
 
 	public void execute(List<List<Punkt>> clusters, ArrayList<LinienSegment> mst, List<LinkedList<Punkt>> tsp) {
 		mst.get(0).SortMstLaenge(mst);
 		
+		for (int i=0; i< clusters.size() -1 ;i++) {
 		LinienSegment mstEdge =  findClosestCluster(clusters, mst, tsp);
 		//verbindet Cluster, behaltet Tsp Eigenschaften bei.
+		}
+		
+		System.out.println("final tsp printing @@@@@@");
+		for (int i=0; i< tsp.get(0).size(); i++) {
+			tsp.get(0).get(i).printPunkt();
+		}
 	}
 
 
@@ -35,7 +44,7 @@ public class ClusterMerge {
 
 
 	private int[] ConnectsTwoClusters(List<List<Punkt>> clusters, LinienSegment ls) {
-		//Test ob die beiden Endpkte NICHT dem selben Cluster gehören
+		//Test ob die beiden Endpkte NICHT dem selben Cluster gehÃ¶ren
 		//1.Cluster finden
 		int a[]= {0,0,0}; 
 		for( int i =0 ; i < clusters.size(); i++) {
@@ -46,87 +55,199 @@ public class ClusterMerge {
 				else {
 					//2. Cluster finden
 					for( int j =0 ; j < clusters.size(); j++) {
-						if (clusters.get(j).contains(ls.endpkt2) == true) {
-							a[0] =1; a[1] = i; a[2] =j;
-							return a;
+						if(i != j) {
+							if (clusters.get(j).contains(ls.endpkt2) == true) {
+								a[0] =1; a[1] = i; a[2] =j;
+								return a;
+							}
 						}
 					}
 				}
 			}
 		}
-		return null;
+		return a;
 	}
 	
 	private LinienSegment ConnectTsp(LinienSegment mstEdge, int i, int j, List<LinkedList<Punkt>> tsp) {
 		LinienSegment bestEdge;
-		ArrayList<LinienSegment> candidate = new ArrayList<LinienSegment>();
-		int minLaenge =0;
-		boolean next;
+	//ArrayList<LinienSegment> candidate = new ArrayList<LinienSegment>();
+//		ListIterator<LinkedList<Punkt>> iterator = tsp.listIterator(); 
+		
 		LinkedList<Punkt> temp= new LinkedList<Punkt>();
 		
 		int a = tsp.get(i).indexOf(mstEdge.endpkt1);
 		int b = tsp.get(j).indexOf(mstEdge.endpkt2);
 		
-//		Punkt aNext = tsp.get(i).listIterator(a).next();
-//		aNext.printPunkt();
-//		System.out.println( tsp.get(i).listIterator(a).hasNext());
+		if ( a == -1) {
+			System.out.println("i und j zuweisung falsch!");
+			System.out.println("a: "+a);
+			System.out.println("b: "+b);
+			a = tsp.get(j).indexOf(mstEdge.endpkt1);
+			b = tsp.get(i).indexOf(mstEdge.endpkt2);
+		}
 		
-		mstEdge.printLs();
+		int w= a+1; int x= a-1; int y= b+1 ; int z= b-1;
 		
 		
-		//reicht das.? nicht kreuzen mit mst!
-		LinienSegment c11 = new LinienSegment(tsp.get(i).listIterator(a).next() , tsp.get(j).listIterator(b).next());
-	//	LinienSegment c11 = new LinienSegment(tsp.get(i).listIterator(a).previous() , tsp.get(j).listIterator(b).next());
-	//	LinienSegment c22 = new LinienSegment(tsp.get(i).listIterator(a).next() ,  tsp.get(j).listIterator(b).previous());
-		LinienSegment c22 = new LinienSegment(tsp.get(i).listIterator(a).previous(), tsp.get(j).listIterator(b).previous());
+		try {
+			tsp.get(i).get(w);
+		}
+		catch(IndexOutOfBoundsException e) {
+			w = 0;
+		}
+		
+		try {
+			tsp.get(i).get(x);
+		}
+		catch(IndexOutOfBoundsException e) {
+			x = tsp.get(i).size()-1;
+		}
+		
+		
+		try {
+			tsp.get(j).get(y);
+		}
+		catch(IndexOutOfBoundsException e) {
+			y = 0;
+		}
+		
+		try {
+			tsp.get(j).get(z);
+		}
+		catch(IndexOutOfBoundsException e) {
+			z = tsp.get(j).size()-1;
+		}
 		
 
-		System.out.println("Die 2 sind meine Kandidaten fürs verbinden, sollten keine mst Knoten haben");
-		System.out.println("@@@@@@@");
-		c11.printLs();
-		c22.printLs();
-		System.out.println("@@@@@@@");
 		
+		LinienSegment c[] = candidate(i,j,w,x,y,z,tsp, mstEdge); //ist nicht kreuzungsfrei
+		LinienSegment c11 = c[0];
+		LinienSegment c22 = c[1];
 		
-		LinienSegment m11 = new LinienSegment(mstEdge.endpkt1 , tsp.get(i).listIterator(a).next() );
-		LinienSegment m12 = new LinienSegment(mstEdge.endpkt1 , tsp.get(i).listIterator(a).previous());
-		LinienSegment m21 = new LinienSegment(mstEdge.endpkt2 , tsp.get(j).listIterator(a).next());
-		LinienSegment m22 = new LinienSegment(mstEdge.endpkt2 , tsp.get(j).listIterator(a).previous());
+		LinienSegment m11 = new LinienSegment(mstEdge.endpkt1, tsp.get(i).get(w));
+		LinienSegment m12 = new LinienSegment(mstEdge.endpkt1, tsp.get(i).get(x));
+		LinienSegment m21 = new LinienSegment(mstEdge.endpkt2, tsp.get(j).get(y));
+		LinienSegment m22 = new LinienSegment(mstEdge.endpkt2, tsp.get(j).get(z));
 		
 		double g0= Math.pow(c11.gewicht - m11.gewicht - m21.gewicht, 2);
-		double g1= Math.pow(c22.gewicht - m12.gewicht - m22.gewicht,2);
-		
+		double g1= Math.pow(c22.gewicht - m12.gewicht - m22.gewicht, 2);
 		
 		
 		if( g0 < g1) {
 			bestEdge = c11;
-			next = true;
-			temp.add(mstEdge.endpkt1);
-			for (int p=0; p< tsp.get(j).size() -1 ; p++) { // -1, den der letzte Punkt soll mit vorherigem mstedge.next
-				temp.add(tsp.get(j).listIterator(b - p).previous());
-			//funktioniert nur mit connected linked list
-			}
+			int count=0;
 			
-			for (int p=0; p< tsp.get(i).size() -1 ; p++) { // -1, den der letzte Punkt soll mit vorherigem mstedge.next
-				temp.add(tsp.get(i).listIterator(a + p).next());
+			for (int p=0; p < tsp.get(j).size() ; p++) {
+				try {
+					temp.add(tsp.get(j).get(b + p));
+				}
+				catch(IndexOutOfBoundsException e) {	//NoSuchElementException
+					temp.add(tsp.get(j).get(count));
+					count++;
+				}
+			}
+			count =0;
+			for (int p=0; p < tsp.get(i).size() ; p++) {
+				try {
+					temp.add(tsp.get(i).get(b + p + 1));
+				}
+				catch(IndexOutOfBoundsException e) {	//NoSuchElementException wenn mit indexIterator
+					
+					temp.add(tsp.get(i).get(count));
+					count++;
+				}
 			}
 		}
 		else {
 			bestEdge = c22;
-			next = false;
+			int count=0;
+			
+			for (int p=0; p < tsp.get(j).size() ; p++) {
+				try {
+					temp.add(tsp.get(j).get(b + p));
+				}
+				catch(IndexOutOfBoundsException e) {	//NoSuchElementException
+					temp.add(tsp.get(j).get(count));
+					count++;
+				}
+			}
+			count =0;
+			for (int p=0; p < tsp.get(i).size() ; p++) {
+				try {
+					temp.add(tsp.get(i).get(b + p + 1));
+				}
+				catch(IndexOutOfBoundsException e) {	//NoSuchElementException wenn mit indexIterator
+					
+					temp.add(tsp.get(i).get(count));
+					count++;
+				}
+			}
 		}
+		
+		
 		tsp.remove(i);
-		tsp.remove(j-1);
+		
+		if( i < j ) {
+			tsp.remove(j-1);
+		}
+		else tsp.remove(j);
 		
 		tsp.add(temp);
-		System.out.println(" size:"+ tsp.get(0).size() );
+		
+		bestEdge.printLs();
+		
 		return bestEdge;
+	}
+
+	private LinienSegment[] candidate(int i, int j,int w, int x, int y, int z, List<LinkedList<Punkt>> tsp, LinienSegment mst) {
+		LinienSegment c[] = {null, null};
+
+		
+		LinienSegment c11 = new LinienSegment(tsp.get(i).get(w), tsp.get(j).get(y));
+		LinienSegment c12 = new LinienSegment(tsp.get(i).get(x), tsp.get(j).get(y));
+		
+		LinienSegment c21 = new LinienSegment(tsp.get(i).get(w), tsp.get(j).get(z));
+		LinienSegment c22 = new LinienSegment(tsp.get(i).get(x), tsp.get(j).get(z));
+		
+		c[0] = kreuzungsfrei(c11,c12, mst);
+	
+//		System.out.println("kreuzungstest @@@@@");	
+//		c11.printLs();
+//		c12.printLs();
+//		c[0].printLs();
+//		System.out.println("kreuzungstest @@@@@");
+		
+		c[1] = kreuzungsfrei(c21,c22, mst);
+
+		return c;
+	}
+private LinienSegment kreuzungsfrei(LinienSegment c1, LinienSegment c2, LinienSegment mst) {
+		//check if c1 und mst sich kreuzen, wenn ja kreuzen sich c2 und mst nicht
+		double t,u;
+		//check ob richtiges ls wiedergegeben
+		double t1= (c1.endpkt1.getX() - mst.endpkt1.getX())*(mst.endpkt1.getY()- mst.endpkt2.getY());
+		double t2= (c1.endpkt1.getY() - mst.endpkt1.getY())*(mst.endpkt1.getX()- mst.endpkt2.getX());
+		
+		double t3 = (c1.endpkt1.getX() - c1.endpkt2.getX())*(mst.endpkt1.getY()- mst.endpkt2.getY());
+		double t4 = (c1.endpkt1.getY() - c1.endpkt2.getY())*(mst.endpkt1.getX()- mst.endpkt2.getX());
+		
+		t = (t1 - t2)/(t3 - t4);
+
+		if (t <= 1 && t >= 0) return c2;
+		
+		double u1= (c1.endpkt2.getX() - c1.endpkt1.getX())*(c1.endpkt1.getY()- mst.endpkt1.getY());
+		double u2= (c1.endpkt2.getY() - c1.endpkt1.getY())*(c1.endpkt1.getX()- mst.endpkt1.getX());
+		
+		u = (u1-u2)/(t3-t4);
+		
+		if (u <= 1 && u >= 0) return c2;
+		
+		return c1;
 	}
 
 	public void ClusterCombine(int i, int j, List<List<Punkt>> clusters) {
 		clusters.get(i).addAll(clusters.get(j));
 		clusters.remove(j);
 	}
-
 
 }
